4c56e9f5411d8bcaa552ea68079e5cb1fc8f814c
yatatsu <yatatsukitagawa@gmail.com>
2016-02-02 13:35:53 +0900

    Change retention policy of annotation

--- autobundle/src/main/java/com/yatatsu/autobundle/AutoBundleField.java
+++ autobundle/src/main/java/com/yatatsu/autobundle/AutoBundleField.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Target(ElementType.FIELD)
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.CLASS)
 public @interface AutoBundleField {
     boolean required() default true;
     String key() default "";
