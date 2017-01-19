5f71d362a12f93a36589e90e316b2e070030be3e
yatatsu <yatatsukitagawa@gmail.com>
2017-01-20 00:55:26 +0900

    Add android-apt example.

---
+++ example-apt/src/main/java/com/yatatsu/autobundle/example/apt/ExampleActivity.java
@@ -0,0 +1,12 @@
+package com.yatatsu.autobundle.example.apt;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.yatatsu.autobundle.AutoBundleField;
+
+
+public class ExampleActivity extends AppCompatActivity {
+
+    @AutoBundleField String name;
+    @AutoBundleField int age;
+}
---
+++ example-apt/src/main/java/com/yatatsu/autobundle/example/apt/ExampleFragment.java
@@ -0,0 +1,12 @@
+package com.yatatsu.autobundle.example.apt;
+
+import android.support.v4.app.Fragment;
+
+import com.yatatsu.autobundle.AutoBundleField;
+
+
+
+public class ExampleFragment extends Fragment {
+
+    @AutoBundleField int id;
+}
